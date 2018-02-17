d6ca95ee6ec2f32230ec1dcb8faf0263b9871de4
Sodre177 <adam@adamwalsh.name>
2018-02-17 16:38:30 +1000

    fixup! Prettifying discrim

--- primcmds.py
+++ primcmds.py
@@ -496,9 +496,9 @@ async def prim_cmd_about(message, cargs, client, conf, botdata):
 async def prim_cmd_discrim(message, cargs, client, conf, botdata):
      p = client.get_all_members()
      found_members = set(filter(lambda m: m.discriminator==cargs, p))
-     user_info = [ (m, m.userid) for m in found_members]
+     user_info = [ (m, m.id) for m in found_members]
      max_len = len(max(list(zip(*user_info))[0],key=len))
-     user_strs = [ "{0[0]:^{max_len}} ({0[1]:^25})".format(user, max_len = max_len) for user in user_info]
+     user_strs = [ "{0[0]!s:^{max_len}} ({0[1]:^25})".format(user, max_len = max_len) for user in user_info]
      await reply(client, message, "```asciidoc\n= Users found =\n{}\n```".format('\n'.join(user_strs)))
 
 
