3fb9b11e9232790558539aff895cedd980556d36
Puerosola <pue@puerosola.pw>
2018-03-15 15:30:12 +1000

    Slight modifications to perm structure

--- paraperms.py
+++ paraperms.py
@@ -79,7 +79,7 @@ async def perm_manager(client, botdata, conf=None, message=None, user=None, serv
     if (user is None) or (conf is None):
         return(2, "An internal error occurred.")
 
-    (execerror, msg) = await permFuncs["exec"][0](client, botdata, conf, message, user, server)
+    (execerror, msg) = await permFuncs["Developer"][0](client, botdata, conf, message, user, server)
     if execerror == 0:
         return (execerror, msg)
     if int(userid) not in conf.getintlist("manager"):
@@ -98,7 +98,7 @@ async def perm_contributor(client, botdata, conf=None, message=None, user=None, 
         if (user is None) or (conf is None):
             return(2, "An internal error occurred.")
 
-        (execerror, msg) = await permFuncs["managers"][0](client, botdata, conf, message, user, server)
+        (execerror, msg) = await permFuncs["manager"][0](client, botdata, conf, message, user, server)
         if execerror == 0:
             return (execerror, msg)
         if int(userid) not in conf.getintlist("contributor"):
