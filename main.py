0415e1e1176b516d28066932d0c517f44e087f10
TotallyTriggeredTigers <28727461+TotallyTriggeredTigers@users.noreply.github.com>
2018-03-17 17:38:23 +1300

    same

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ bot.load_cmds("commands/testcmds.py")
 bot.load_cmds("commands/util_cmds.py")
 bot.load_cmds("commands/exec_cmds.py")
 bot.load_cmds("commands/help_cmds.py")
-bot.load_cmds("commmands/admin_cmds.py")
+bot.load_cmds("commands/admin_cmds.py")
 
 bot.objects["invite_link"] = "https://discordapp.com/api/oauth2/authorize?client_id=401613224694251538&permissions=8&scope=bot"
 bot.objects["support guild"] = "https://discord.gg/ECbUu8u"
