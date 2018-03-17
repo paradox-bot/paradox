ed6b865869701262ca6d06a3c4541d5e060831a2
Sodre177 <adam@adamwalsh.name>
2018-03-17 20:12:30 +1000

    Event handler file

---
+++ events/join_events.py
@@ -0,0 +1,43 @@
+from contextBot.Context import Context
+
+
+def load_into(bot):
+    @bot.client.event
+    async def on_member_join(member):
+        ctx = Context(bot=bot, member=member)
+        if not ctx.bot.serv_conf["join"].get(ctx.data, ctx.server):
+            return
+
+        ch = ctx.bot.serv_conf["join_ch"].get(ctx.data, ctx.server)
+        msg = ctx.bot.serv_conf["join_msg"].get(ctx.data, ctx.server)
+
+        if not ch:
+            return
+
+        ch = ctx.server.get_channel(ch)
+
+        if not ch:
+            return
+
+        msg = await ctx.para_format(msg)
+        await ctx.client.send_message(ch, msg)
+
+    @bot.client.event
+    async def on_member_remove(member):
+        ctx = Context(bot=bot, member=member)
+        if not ctx.bot.serv_conf["leave"].get(ctx.data, ctx.server):
+            return
+
+        ch = ctx.bot.serv_conf["leave_ch"].get(ctx.data, ctx.server)
+        msg = ctx.bot.serv_conf["leave_msg"].get(ctx.data, ctx.server)
+
+        if not ch:
+            return
+
+        ch = ctx.server.get_channel(ch)
+
+        if not ch:
+            return
+
+        msg = await ctx.para_format(msg)
+        await ctx.client.send_message(ch, msg)
