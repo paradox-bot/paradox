e89973fd7ab06d09f17ec47c2c95da623451052f
Sodre177 <adam@adamwalsh.name>
2018-03-22 09:47:11 +1000

    Corrected config references in events

--- events/join_events.py
+++ events/join_events.py
@@ -5,11 +5,11 @@ def load_into(bot):
     @bot.event
     async def on_member_join(member):
         ctx = Context(bot=bot, member=member)
-        if not ctx.bot.serv_conf["join"].get(ctx.data, ctx.server):
+        if not ctx.server_conf.join.get(ctx.data, ctx.server):
             return
 
-        ch = ctx.bot.serv_conf["join_ch"].get(ctx.data, ctx.server)
-        msg = ctx.bot.serv_conf["join_msg"].get(ctx.data, ctx.server)
+        ch = ctx.server_conf.join_ch.get(ctx)
+        msg = ctx.server_conf.join_msg.get(ctx)
 
         if not ch:
             return
@@ -20,16 +20,16 @@ def load_into(bot):
             return
 
         msg = await ctx.ctx_format(msg)
-        await ctx.client.send_message(ch, msg)
+        await ctx.bot.send_message(ch, msg)
 
     @bot.event
     async def on_member_remove(member):
         ctx = Context(bot=bot, member=member)
-        if not ctx.bot.serv_conf["leave"].get(ctx.data, ctx.server):
+        if not ctx.server_conf.leave.get(ctx):
             return
 
-        ch = ctx.bot.serv_conf["leave_ch"].get(ctx.data, ctx.server)
-        msg = ctx.bot.serv_conf["leave_msg"].get(ctx.data, ctx.server)
+        ch = ctx.server_conf.leave_ch.get(ctx)
+        msg = ctx.server_conf.leave_msg.get(ctx)
 
         if not ch:
             return
@@ -40,4 +40,4 @@ def load_into(bot):
             return
 
         msg = await ctx.ctx_format(msg)
-        await ctx.client.send_message(ch, msg)
+        await ctx.bot.send_message(ch, msg)
