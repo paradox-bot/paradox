828a8a5ffad6cf9c228db0d777c239eb920cc333
Sodre177 <adam@adamwalsh.name>
2018-03-22 23:54:45 +1000

    Modifed join events to use proper settings

--- events/join_events.py
+++ events/join_events.py
@@ -6,8 +6,6 @@ def load_into(bot):
     @bot.event
     async def on_member_join(member):
         ctx = Context(bot=bot, member=member)
-        if not (await ctx.server_conf.join.get(ctx.data, ctx.server)):
-            return
 
         autorole = await ctx.server_conf.guild_autorole.get(ctx)
         autorole = discord.utils.get(ctx.server.roles, id=autorole)
@@ -20,36 +18,35 @@ def load_into(bot):
                 """
                 pass
 
-        ch = await ctx.server_conf.join_ch.get(ctx)
-        msg = await ctx.server_conf.join_msg.get(ctx)
+        if (await ctx.server_conf.join_msgs_enabled.get(ctx.data, ctx.server)):
+            ch = await ctx.server_conf.join_ch.get(ctx)
+            msg = await ctx.server_conf.join_msg.get(ctx)
 
-        if not ch:
-            return
+            if not ch:
+                return
 
-        ch = ctx.server.get_channel(ch)
+            ch = ctx.server.get_channel(ch)
 
-        if not ch:
-            return
+            if not ch:
+                return
 
-        msg = await ctx.ctx_format(msg)
-        await ctx.bot.send_message(ch, msg)
+            msg = await ctx.ctx_format(msg)
+            await ctx.bot.send_message(ch, msg)
 
     @bot.event
     async def on_member_remove(member):
         ctx = Context(bot=bot, member=member)
-        if not (await ctx.server_conf.leave.get(ctx)):
-            return
+        if (await ctx.server_conf.leave_msgs_enabled.get(ctx)):
+            ch = await ctx.server_conf.leave_ch.get(ctx)
+            msg = await ctx.server_conf.leave_msg.get(ctx)
 
-        ch = await ctx.server_conf.leave_ch.get(ctx)
-        msg = await ctx.server_conf.leave_msg.get(ctx)
+            if not ch:
+                return
 
-        if not ch:
-            return
+            ch = ctx.server.get_channel(ch)
 
-        ch = ctx.server.get_channel(ch)
+            if not ch:
+                return
 
-        if not ch:
-            return
-
-        msg = await ctx.ctx_format(msg)
-        await ctx.bot.send_message(ch, msg)
+            msg = await ctx.ctx_format(msg)
+            await ctx.bot.send_message(ch, msg)
