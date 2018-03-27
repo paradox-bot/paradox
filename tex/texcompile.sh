01c9137fc598030ac80ad61f312f1d9be3dfc236
PueChoc <pue@puerosola.pw>
2018-03-28 00:08:24 +1000

    Improved tex compiling

--- tex/texcompile.sh
+++ tex/texcompile.sh
@@ -1,3 +1,6 @@
+#cd tex
+#pdflatex $1.tex >> texout.log 2>&1
+#convert -background white -flatten -border 80 -density 500 -quality 100 $1.pdf $1.png
 cd tex
-pdflatex $1.tex >> texout.log 2>&1
-convert -background white -flatten -border 80 -density 500 -quality 100 $1.pdf $1.png
+latex -no-shell-escape -halt-on-error $1.tex >> $1.texout.log 2>&1
+dvipng -bg Whit-T tight -D 620 -o $1.png $1
