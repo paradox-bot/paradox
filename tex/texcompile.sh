d149228e46be9e4a844611802ec56201a76541b1
PueChoc <pue@puerosola.pw>
2018-03-28 00:24:32 +1000

    Reverted tex modifications

--- tex/texcompile.sh
+++ tex/texcompile.sh
@@ -1,6 +1,6 @@
-#cd tex
-#pdflatex $1.tex >> texout.log 2>&1
-#convert -background white -flatten -border 80 -density 500 -quality 100 $1.pdf $1.png
 cd tex
-latex -no-shell-escape -halt-on-error $1.tex >> $1.texout.log 2>&1
-dvipng -bg White -T tight -D 620 -o $1.png $1
+pdflatex $1.tex >> texout.log 2>&1
+convert -background white -flatten -border 80 -density 500 -quality 100 $1.pdf $1.png
+#cd tex
+#latex -no-shell-escape -halt-on-error $1.tex >> $1.texout.log 2>&1
+#dvipng -bg White -T tight -D 620 -o $1.png $1
