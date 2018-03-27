9db7a855f9fe84adb951b9e1b9b865008ca05417
PueChoc <pue@puerosola.pw>
2018-03-28 00:14:39 +1000

    fixup!(typo) Improved tex compiling

--- tex/texcompile.sh
+++ tex/texcompile.sh
@@ -3,4 +3,4 @@
 #convert -background white -flatten -border 80 -density 500 -quality 100 $1.pdf $1.png
 cd tex
 latex -no-shell-escape -halt-on-error $1.tex >> $1.texout.log 2>&1
-dvipng -bg Whit-T tight -D 620 -o $1.png $1
+dvipng -bg White -T tight -D 620 -o $1.png $1
