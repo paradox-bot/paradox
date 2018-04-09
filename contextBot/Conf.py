6a3b08c6ad3ec109888924d2c376102dc3df8643
PueChoc <pue@puerosola.pw>
2018-04-09 19:58:13 +1000

    Removing the contextBot folder which is now a repo

--- contextBot/Conf.py
+++
@@ -1,8 +0,0 @@
-class Conf:
-    def __init__(self, name):
-        self.name = name
-        self.settings = {}
-
-    def setting(self, cls):
-        self.settings[cls.name] = cls
-        setattr(self, cls.name, cls)
