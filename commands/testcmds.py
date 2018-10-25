14ca692b9cb0c22e1b17b0e3efa05d5d601abf75
PueChoc <pue@puerosola.pw>
2018-10-25 19:28:00 +1100

    Testcmd has simultaneous message and reaction awaiting

--- commands/testcmds.py
+++ commands/testcmds.py
@@ -1,5 +1,6 @@
 # from contextBot.CommandHandler import CommandHandler
 from paraCH import paraCH
+import asyncio
 
 cmds = paraCH()
 
@@ -11,6 +12,14 @@ async def cmd_test(ctx):
         -a:: blah
         -b:: blaefd
     """
-    async def test(bot):
-        await bot.log("test")
-    await ctx.bot.schedule(await ctx.from_now(10), test, 0)
+    async def for_edits():
+        await ctx.reply("Listening for edits")
+        (before, after) = await ctx.bot.wait_for_message_edit(message=ctx.msg, timeout=300, check=lambda before, after: True)
+        await ctx.reply("Got edit!\nBefore: `{}`\nAfter: `{}`".format(before.content, after.content))
+
+    async def for_reactions():
+        out_msg = await ctx.reply("Listening for reactions on this message")
+        res = await ctx.bot.wait_for_reaction(message=out_msg, timeout=300)
+        await ctx.reply("Got reaction: {}".format(res.reaction.emoji))
+    asyncio.ensure_future(for_edits(), loop = ctx.bot.loop)
+    asyncio.ensure_future(for_reactions(), loop = ctx.bot.loop)
