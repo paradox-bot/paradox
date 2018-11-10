48abacf1bff8c388525f4c0ea19c6361bad40218
PueChoc <pue@puerosola.pw>
2018-11-11 03:09:01 +1100

    Update invite

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ Bot.log = log
 
 bot.load("commands", "config", "events", "utils", ignore=["RCS", "__pycache__"])
 
-bot.objects["invite_link"] = "http://invite.paradoxical.pw"
+bot.objects["invite_link"] = "https://discordapp.com/api/oauth2/authorize?client_id=510789298321096704&permissions=370752&scope=bot"
 bot.objects["support guild"] = "https://discord.gg/ECbUu8u"
 bot.objects["sorted cats"] = ["General",
                               "Fun Stuff",
