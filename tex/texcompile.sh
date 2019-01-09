471ffb0d310a7e7dcd7c032b63f3eed59fb19b7d
PueChoc <pue@puerosola.pw>
2019-01-09 12:51:43 +1000

    Hacky sandbox

--- tex/texcompile.sh
+++ tex/texcompile.sh
@@ -1,5 +1,13 @@
 cd tex
-pdflatex $1.tex >> texout.log 2>&1
+mkdir -p staging/$1/
+
+cp $1.tex staging/$1/
+cp fancycom.sty staging/$1/
+
+cd staging/$1/
+chmod -R o+rwx .
+
+sudo -u latex pdflatex -no-shell-escape $1.tex >> texout.log 2>&1
 if [ $? != 0 ];
 then
     grep -A 10 -m 1 "^!" $1.log;
@@ -11,6 +19,9 @@ if [ ! -f $1.pdf ];
 then
   exit
 fi
+
+mv $1.pdf ../../
+cd ../../
 
 convert -background white -flatten -border 80 -density 500 -quality 300 -trim +repage -border 40 $1.pdf $1.png;
 
@@ -52,7 +63,7 @@ minwidth=1000
 newwidth=$(( width > minwidth ? width : minwidth ))
 
 convert -background transparent -extent ${newwidth}x${height} $1.png $1.png
-rm $1.pdf
+rm -f $1.pdf
 #cd tex
 #latex -no-shell-escape -halt-on-error $1.tex >> $1.texout.log 2>&1
 #dvipng -bg White -T tight -D 620 -o $1.png $1
